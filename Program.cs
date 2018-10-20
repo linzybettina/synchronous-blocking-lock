9621c492d2abccd3653f6ba9e25234f3167013ef
linzybettina <43557782+linzybettina@users.noreply.github.com>
2018-10-20 08:18:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    decimal totalBalance = 50000;
+    private Object myLock = new Object();
+
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.WithDraw(5000);
+        Console.ReadKey();
+    }
+
+    public void WithDraw(decimal amount)
+    {
+        lock (myLock)
+        {
+            if (amount > totalBalance)
+            {
+                Console.WriteLine("Insufficient Amount.");
+            }
+
+            totalBalance -= amount;
+            Console.WriteLine("Total Balance {0}", totalBalance);
+        }
+    }
+
+}   
